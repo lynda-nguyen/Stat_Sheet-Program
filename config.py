2224271932d88e5e400168013ecdfe648daf1cc3
lynda-nguyen <36965203+lynda-nguyen@users.noreply.github.com>
2019-09-23 06:54:25 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+# Current: 2019-2020 players list  
+
+players_list = ['TC', 'LR', 'TB', 'AOF', 'AS', 'AJ', 'DM', 'EK', 'CK', 'TG', 'ME', 'CB', 'KB']
+action_list = ['FG', 'FGA', '3FG', '3FGA', 'FT', 'FTA', 'OREB', 'DREB', 'TREB', 'PF', 'A', 'TO', 'BLK', 'STL', 'PTS']
